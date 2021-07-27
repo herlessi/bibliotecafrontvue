d5494a4efff3290179a6e95441c69b58e7efd7e7
herlessi <herlessi@gmail.com>
2021-07-26 21:17:17 -0300

    Chamados

--- src/componentes/Chamados.vue
+++ src/componentes/Chamados.vue
@@ -1,39 +1,22 @@
 <template>
     <div>
         
-        <div  class="corpochamados">
-            <!-- <v-card class="p-0 ma-2"  
-                style="padding:0px!important;width:100%;">
-                <v-row class="ma-0" 
-                 style="max-width: 100%;padding:5px 10px;display:flex;align-items:center;justify-content:flex-end;">
-                    <v-text-field
-                        class="inputfiltrolistagem"
-                        type="text" label="Nome" v-model="filtronome" >
-                    </v-text-field>
+        <div v-if="pagina == 'datatable'" class="corpochamados">
+            <v-card class="p-0 ma-2" style="padding:0px!important;width:100%;">
+                <v-row class="ma-0" style="max-width: 100%;padding:15px 10px;display:flex;align-items:center;justify-content:flex-end;">
+                   <v-btn color="error" @click="setarModal()" style="margin-right:20px;">
+                        modal
+                    </v-btn>
 
-                    <v-text-field
-                        class="inputfiltrolistagem"
-                        type="text" label="Sobrenome" v-model="filtrosobrenome" >
-                    </v-text-field>
-
-                    <v-text-field
-                        class="inputfiltrolistagem"
-                        type="text" label="Apelido" v-model="filtroapelido" >
-                    </v-text-field>
-
-                    <v-text-field
-                        class="inputfiltrolistagem"
-                        type="text" label="Email" v-model="filtroemail" >
-                    </v-text-field>
-
-                    <v-btn @click="buscaReservas()" small color="light-blue" 
-                        class="botaolistagem"  >
-                        <v-icon color="white">mdi-magnify</v-icon>
-                        Pesquisar
+                    <v-btn color="info" @click="pagina = 'novochamado'">
+                        <v-icon medium color="white" >
+                            mdi-plus
+                        </v-icon>
+                       Abrir novo Chamado
                     </v-btn>
 
                 </v-row>
-            </v-card> -->
+            </v-card>
             <v-data-table v-if="itens.length > 0"
                 :headers="headers"
                 :items="itens"
@@ -49,8 +32,39 @@
 
         </div>
 
+        <div v-if="pagina == 'novochamado'" class="corpochamados">
+            <div class="loginlinha">
+                <v-select :items="areas" v-model="area" @change="carregarAreas()" item-value="id" item-text="descricao" label="Area" outlined >
+                </v-select>
+                <!-- <v-text-field label="Area" class="logininput" type="text" id="login" name="login" v-model="senhaconfirmacao" /> -->
+            </div>
+
+            <div class="loginlinha">
+                <v-select :items="categorias" v-model="categoria" item-value="id" item-text="descricao" label="Categoria" outlined ></v-select>
+                <!-- <v-text-field label="Categoria" class="logininput" type="text" id="login" name="login" v-model="senhaconfirmacao" /> -->
+            </div>
+
+            <div class="loginlinha" >
+                 <v-textarea outlined name="input-7-4" label="Mensagem" v-model="mensagem">
+
+                 </v-textarea>
+                <!-- <v-text-field label="texto" class="logininput" type="text" id="login" name="login" v-model="senhaconfirmacao" /> -->
+            </div>
+
+            <div class="loginlinha">
+                <v-btn class="info" @click="atualizar()" >Salvar</v-btn>
+            </div>
+            
+            <div class="loginlinha">
+                <v-btn class="error" @click="atualizar()" >Cancelar</v-btn>
+            </div>
 
 
+        </div>
+
+        <Modal v-bind:exibirModal="exibirModal" 
+            v-bind:titulo="'Novo Chamado'" v-bind:texto="'Aqui voce pode abrir um novo chamado'"
+            @setarModal="()=>exibirModal = !exibirModal" />
     </div>
 
     
@@ -59,23 +73,40 @@
 </template>
 
 <script>
+
+import Modal from './Modal.vue'
 export default {
+    components:{
+        Modal
+    },
     name: 'listagem',
     props:["recuperarsenha","idusuario"],
     data: () => {
         return {
             headers: [],
-            itens:[]
+            itens:[],
+            exibirModal:false,
+            pagina:'novochamado',
+            // pagina:'datatable',
+            areas: [],
+            area:null,
+            categorias: [],
+            categoria:null,
+            mensagem:null
         }
     },
     watch: {
       headers: (valor) => {
         this.headers = valor
       }
+      
     },
     methods:{
+        setarModal(){
+            this.exibirModal = !this.exibirModal
+        },
         carregarchamados(){
-  
+            // alert(1)
             let idusuario = 43
             console.log("Carregando dados")
             this.$http.get("/chamados?idusuario="+idusuario)
@@ -100,10 +131,28 @@ export default {
                 .catch((error) =>{
                     this.$alert(error);
                 })
+        },
+        carregarAreas(){
+            this.$http.get("/chamados/areas").then(resp =>{
+                this.areas = resp.data
+                // alert(this.area)
+                if(this.area){
+                    this.carregarCategorias(this.area)
+                }else{
+                    this.carregarCategorias(resp.data[0].id)
+                }
+            })
+        },
+        carregarCategorias(idcategoria){
+            // alert("/chamados/categorias/"+idcategoria)
+            this.$http.get("/chamados/categorias/"+idcategoria).then(resp =>{
+                this.categorias = resp.data
+            })
         }
     },
     mounted () {
         this.carregarchamados()
+        this.carregarAreas()
     },
 }
 </script>
@@ -111,6 +160,13 @@ export default {
 <style scoped>
 
    
+    .loginlinha{
+        display:flex;
+        flex-direction: column;
+        width:100%;
+        padding:5px;
+        margin-bottom: 5px;
+    }
 
     .corpochamados{
         background-color:pink;
---
+++ src/componentes/Modal.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="text-center">
+    <v-dialog
+      v-model="exibir"
+      width="500"
+    >
+      <!-- <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="red lighten-2"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Click Me
+        </v-btn>
+      </template> -->
+
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-2">
+          {{titulo}}
+        </v-card-title>
+
+        <v-card-text style="padding-top:20px;">
+          {{texto}}
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" @click="$emit('setarModal')" > OK </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+
+  </div>
+</template>
+
+<script>
+  export default {
+    props:["exibirModal", "titulo", "texto"],
+    data () {
+      return {
+        exibir:false
+      }
+    },
+    watch: {
+      exibirModal(valor) {
+        this.exibir = valor
+      }
+    },
+    methods:{
+
+    },
+    created(){
+        this.exibir = this.exibirModal
+    }
+  }
+</script>
